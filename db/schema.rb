1d6e74c9e8b9d98f805931313d87fe290b62d517
MclPio <113801014+MclPio@users.noreply.github.com>
2023-10-11 21:32:33 -0400

    complete test run

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_012046) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "link"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
