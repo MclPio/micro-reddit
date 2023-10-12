a5e8baa8596168be176ea0198e843c0cba5e19e9
MclPio <113801014+MclPio@users.noreply.github.com>
2023-10-11 21:03:02 -0400

    add uniqueness validation to user emails

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
-  validates_associated :user
-  validates_associated :post
   validates :user_id, presence: true
   validates :post_id, presence: true
+  validates_associated :user
+  validates_associated :post
   belongs_to :post
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
   validates :password, presence: true, length: { in: 12..50 }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_many :posts
   has_many :comments
